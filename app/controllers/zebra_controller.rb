ad43044c1d5aedb602c5c8ce9710e1e67a2516bc
Rebecca Herrera <133678924+reherr@users.noreply.github.com>
2024-05-31 20:04:14 +0000

    scissors and paper templates

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -10,15 +10,35 @@ class ZebraController < ApplicationController
     else
       @outcome = "won"
     end
-    
+
     render({ :template => "game_templates/play_rock"})
   end
 
   def elephant
+    moves = ["rock", "paper", "scissors"]
+    @comp_move = moves.sample
+
+    if @comp_move == "paper"
+      @outcome = "tied"
+    elsif @comp_move == "scissors"
+      @outcome = "lost"
+    else
+      @outcome = "won"
+    end
     render({ :template => "game_templates/play_paper"})
   end
 
   def lion 
+    moves = ["rock", "paper", "scissors"]
+    @comp_move = moves.sample
+
+    if @comp_move == "scissors"
+      @outcome = "tied"
+    elsif @comp_move == "rock"
+      @outcome = "lost"
+    else
+      @outcome = "won"
+    end
     render({ :template => "game_templates/play_scissors"})
   end
 
