61029301ea17d04eb405b978f236f6659248d693
Rebecca Herrera <133678924+reherr@users.noreply.github.com>
2024-05-31 19:50:46 +0000

    nav bar in layout template

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -1,5 +1,16 @@
 class ZebraController < ApplicationController
   def giraffe
+    moves = ["rock", "paper", "scissors"]
+    @comp_move = moves.sample
+
+    if @comp_move == "rock"
+      @outcome = "tied"
+    elsif @comp_move == "paper"
+      @outcome = "lost"
+    else
+      @outcome = "won"
+    end
+    
     render({ :template => "game_templates/play_rock"})
   end
 
