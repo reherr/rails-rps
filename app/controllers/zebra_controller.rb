6b9947eb7c74a7d9c74e961e211968a37c0e0e68
Rebecca Herrera <133678924+reherr@users.noreply.github.com>
2024-05-31 19:32:17 +0000

    created game templates and defined actions

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -1,6 +1,17 @@
 class ZebraController < ApplicationController
   def giraffe
     render({ :template => "game_templates/play_rock"})
+  end
 
+  def elephant
+    render({ :template => "game_templates/play_paper"})
+  end
+
+  def lion 
+    render({ :template => "game_templates/play_scissors"})
+  end
+
+  def rules
+    render({ :template => "game_templates/rules"})
   end
 end
